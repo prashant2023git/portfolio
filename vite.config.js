3d0fe54021fe8dfa474d82d445d83e1596d8f0df
Prashant Kumar <pk4473464@gmail.com>
2025-09-20 02:08:36 +0530

    base path correction

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-website/",
   plugins: [react()],
+  base: "./"
 });
